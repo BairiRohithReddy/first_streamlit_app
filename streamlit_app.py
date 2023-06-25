8f2ab7432da4977aafda89ddc3ce12256332f3a6
BairiRohithReddy <72658371+BairiRohithReddy@users.noreply.github.com>
2023-06-24 21:36:36 -0500

    Update streamlit_app.py
    completely wrapped the rest of the code inside of a function

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,8 +59,15 @@ if streamlit.button('get fruit list load'):
   streamlit.dataframe(my_data_rows)
 
 
-streamlit.stop()
+#streamlit.stop()
+
 #allowing the user to add a fruit of his choice
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values('from streamlit')");
+    return "Thanks for adding:" + new_fruit
 add_my_fruit = streamlit.text_input("What Fruit Would You Like to Add?")
-streamlit.write("Thanks for adding:",add_my_fruit)
-my_cur.execute("insert into fruit_load_list values('from streamlit')");
+if streamlit.button('add a fruit to the list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
